dabff799fdb5c5ed9532e7e77c33d784a0a35d7d
Liqi Chen <qwer664243654@gmail.com>
2018-11-08 18:26:30 -0500

    Updated with Plane Fitting

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,8 @@
 import csv, math
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 class Plane:
     def __init__(self, x, y, z):
@@ -50,4 +53,39 @@ while start_point > -5:
 
     print("[{}, {}, {}]".format(x,y,z))
     v = A.dot(np.array(v))
+
+# Plane Fitting
+A = np.matrix([[i[0], i[1], 1] for i in points])
+B = np.matrix([[i[2]] for i in points])
+param = (A.T * A).I * A.T * B
+
+# Plot Plane
+xs = []
+ys = []
+zs = []
+for i in points:
+    xs.append(i[0])
+    ys.append(i[1])
+    zs.append(i[2])
+
+# plot raw data
+plt.figure()
+ax = plt.subplot(111, projection='3d')
+ax.scatter(xs, ys, zs, color='b')
+
+# plot plane
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+X,Y = np.meshgrid(np.arange(xlim[0], xlim[1]),
+                  np.arange(ylim[0], ylim[1]))
+Z = np.zeros(X.shape)
+for r in range(X.shape[0]):
+    for c in range(X.shape[1]):
+        Z[r,c] = param[0] * X[r,c] + param[1] * Y[r,c] + param[2]
+ax.plot_wireframe(X,Y,Z, color='k')
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+plt.show()
 
