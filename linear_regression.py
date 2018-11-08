b6db3864d2ca6dcf1a2ca65fe80feb7809e12b9f
Ben Arbib <ba2490@columbia.edu>
2018-11-08 13:47:23 -0500

    added probe array

--- linear_regression.py
+++ linear_regression.py
@@ -32,33 +32,13 @@ points = []
 start_point = 5
 v = np.array([0, math.cos(25), math.sin(25)])
 A = np.array([[math.cos(angle), 0, math.sin(angle)],[0,1,0],[-math.sin(angle), 0, math.cos(angle)]])
-while start_point >= -5:
+probe_coord = []
+while start_point > -5:
+    probe_coord.append([0, -1, start_point])
     start_point -= 0.5
-    dis = distance.pop()
-    x, y, z = v[0], v[1], v[2]
-    points.append([x*dis,y*dis,z*dis])
-    v = A.dot(np.array(v))
-
-
-
-
-'''
-Ben assignmen
-assume start of probe at 0,-1,5 to 0,-1,-5
-every loop moves z -0.5
-facing the y axis 0,1,0 
-tilting angle = 25
-10 samples per rev
-2 revs in total between 0, -1, 5 to 0, -1, -5
-
-time 0 = change angle = 0, cos25, sin25
-time 1 = (change by 36 degree)
-every loop left multiply prev vector by rotational matrix   [cos  0 sin ] (matrix on the left)
-                                                        [0    1   0 ]
-                                                        [-sin 0 cos]
-Mv(t) * dis + 0,-1, 5-(0.5)*t
-that gives us new coordinate of point at time t
-at time t multiply dis by 
-
-E stem M step 
-'''
+    if len(distance) > 0:
+        dis = distance.pop()
+        x, y, z = v[0], v[1], v[2]
+        points.append([x*float(dis),y*float(dis),z*float(dis)])
+        # print("{} {} {}".format(x*float(dis),y*float(dis),z*float(dis)))
+        v = A.dot(np.array(v))
