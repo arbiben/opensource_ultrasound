9d46517a0834b574214ba492ed1832be0086487b
Ben Arbib <ba2490@columbia.edu>
2018-11-08 15:29:35 -0500

    remove multi in print statement

--- linear_regression.py
+++ linear_regression.py
@@ -33,15 +33,21 @@ start_point = 5
 v = np.array([0, math.cos(25*math.pi/180), math.sin(25*math.pi/180)])
 A = np.array([[math.cos(angle), 0, math.sin(angle)],[0,1,0],[-math.sin(angle), 0, math.cos(angle)]])
 probe_coord = []
+
 while start_point > -5:
     probe_coord.append([0, -1, start_point])
+
+    div = math.sqrt(v[0]**2+v[1]**2+v[2]**2)
+    unit_v = [v[0]/div, v[1]/div, v[2]/div]
+
     start_point -= 0.5
-    # if len(distance) > 0:
     dis = distance.pop()
-    x = probe_coord[-1][0] + (v[0]*float(dis))
-    y = probe_coord[-1][1] + (v[1]*float(dis))
-    z = probe_coord[-1][2] + (v[2]*float(dis))
+    x = probe_coord[-1][0] + (unit_v[0]*float(dis))
+    y = probe_coord[-1][1] + (unit_v[1]*float(dis))
+    z = probe_coord[-1][2] + (unit_v[2]*float(dis))
+
     points.append([x,y,z])
-    # print("[{}, {}, {}]".format(x*float(dis),y*float(dis),z*float(dis)))
+
+    print("[{}, {}, {}]".format(x,y,z))
     v = A.dot(np.array(v))
-    
+
